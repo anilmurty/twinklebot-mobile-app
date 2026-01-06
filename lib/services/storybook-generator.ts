62bc98bc8208aef3fc52b55ce10afbc41898ea4e
Anil Murty <anil.murty@gmail.com>
2026-01-06 15:41:19 -0800

    Fix const reassignment error - change characterVariationUrl to let

--- lib/services/storybook-generator.ts
+++ lib/services/storybook-generator.ts
@@ -203,7 +203,7 @@ export async function generateStorybook(storybookId: string): Promise<void> {
       }
 
       // Always use front variation for all scenes (we only generate one variation now)
-      const characterVariationUrl = variations.front_variation_url
+      let characterVariationUrl = variations.front_variation_url
 
       if (!characterVariationUrl) {
         throw new Error(`Character variation URL not found`)
